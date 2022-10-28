5658e56fa19c4436f2a498df614df60ffb50285d
Jonathan Gray <zeusdcs@gmail.com>
2022-10-27 23:45:17 -0400

    fixed cors function

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ const
     app = express(),
     PORT = process.env.PORT || 5000;
 
-app.use(cors, {
+app.use(cors({
     origin: "https://express-app-coral.vercel.app" 
-})
+}))
 
 app.use(express.static("public"))
 
